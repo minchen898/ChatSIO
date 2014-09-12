2995366258ff8a3db44ecda31ae84ecd941e0c2c
minchen898 <minchen898@gmail.com>
2014-09-12 18:26:19 +0800

    Beta #1
    Chat
    Welcome message
    Join and leave message

--- service/room.js
+++ service/room.js
@@ -2,12 +2,16 @@ var members = require('./roomembers');
 
 exports.onMemberJoin = function(socket, name) {
   members.join(socket.id, name);
+  socket.emit('deliver', 'Welcome ' + name);
+  socket.broadcast.emit('member_join', name);
 };
 
 exports.onMemberLeave = function(socket) {
   members.leave(socket.id);
+  socket.broadcast.emit('member_leave', members.getName(socket.id));
 };
 
 exports.onNewMessage = function(socket, message) {
   socket.emit('deliver', members.getName(socket.id) + ' : ' + message);
+  socket.broadcast.emit('deliver', members.getName(socket.id) + ' : ' + message);
 };
--- service/roomembers.js
+++ service/roomembers.js
@@ -2,8 +2,6 @@ var memberName = [];
 
 exports.join = function(id, name) {
   memberName[id] = name;
-  console.log("MEMBER JOIN");
-  console.log(memberName);
 }
 
 exports.getName = function(id) {
@@ -12,6 +10,4 @@ exports.getName = function(id) {
 
 exports.leave = function(id) {
   delete memberName[id];
-  console.log("MEMBER LEAVE");
-  console.log(memberName);
 }
