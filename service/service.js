858c1e3e6f0a6debfd107d976a4cf5641687048a
minchen898 <minchen898@gmail.com>
2014-09-12 15:31:36 +0800

    Send and receive message test #1

--- service/service.js
+++ service/service.js
@@ -3,7 +3,6 @@ var io = require('socket.io');
 
 var server = http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World');
   res.end();
 });
 
@@ -11,9 +10,7 @@ server.listen(8002);
 
 var server_io = io.listen(server);
 server_io.sockets.on('connection', function(socket){
-  //setInterval(function() {
-  //  socket.emit('date', {'date': new Date()});
-  //}, 1000);
-  console.log('Connect!');
-  console.log(socket);
+  sockets.on('message', function(message){
+    socket.emit('broadcast', message);
+  });
 });
