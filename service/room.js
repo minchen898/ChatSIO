5d1d2847b0c7de8799711efcb552ee75c351f219
minchen898 <minchen898@gmail.com>
2014-09-15 12:15:42 +0800

    Member list
    Show member list
    Fix some bugs

--- service/room.js
+++ service/room.js
@@ -1,15 +1,20 @@
+var io = require('socket.io');
+
 var members = require('./roomembers');
 
 exports.onMemberJoin = function(socket, name) {
   members.join(socket.id, name);
   socket.emit('deliver', 'Welcome ' + name);
   socket.broadcast.emit('member_join', name);
+  io.sockets.emit('member_list', members.memberList);
 };
 
 exports.onMemberLeave = function(socket) {
   var name = members.getName(socket.id);
-  members.leave(socket.id);
-  socket.broadcast.emit('member_leave', name);
+  if(name) {
+    members.leave(socket.id);
+    socket.broadcast.emit('member_leave', name);
+  }
 };
 
 exports.onNewMessage = function(socket, message) {
--- service/roomembers.js
+++ service/roomembers.js
@@ -1,7 +1,10 @@
 var memberName = [];
+var list = [];
 
 exports.join = function(id, name) {
   memberName[id] = name;
+  list.push(name);
+  list.sort();
 }
 
 exports.getName = function(id) {
@@ -9,5 +12,10 @@ exports.getName = function(id) {
 }
 
 exports.leave = function(id) {
+  list.splice(list.indexOf(memberName[id]), 1);
   delete memberName[id];
+}
+
+exports.memberList = function() {
+  return list;
 }
